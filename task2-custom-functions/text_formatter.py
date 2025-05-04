e4e2c06f2232cc60144cde8c1d245cfa3b880f1a
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 17:45:03 +0200

    formating the text

--- task2-custom-functions/text_formatter.py
+++ task2-custom-functions/text_formatter.py
@@ -12,3 +12,17 @@ def format_text (text:str,prefix:str="",suffix:str="",capitalize : bool=False,ma
             raise TypeError("max_length must be an integer")
         if max_length<=0:
             raise ValueError("max_length must be a positive integer")
+ formatted=text.strip()
+    if capitalize:
+        formatted=formatted.capitalize()
+        formatted=f"{prefix}{formatted}{suffix}"
+        if max_length is not None:
+            formatted=formatted[:max_length]
+        return formatted
+ 
+ip = input("Please enter a text to format: ")
+prefix = input("Enter Prefix: ")
+suffix = input("Enter suffix: ")
+print(format_text(ip))
+print(format_text(ip,prefix=prefix,suffix=suffix,capitalize=True))        
+print(format_text("sampletext",max_length=8))
