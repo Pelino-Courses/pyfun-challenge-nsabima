3a823c430dfceae8b08cf2b550cf0c240d75b05a
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 17:41:35 +0200

    calucalating date difference

--- task1-package-management/date_calculator.py
+++ task1-package-management/date_calculator.py
@@ -4,3 +4,14 @@ def diference(date1,date2):
  
  datediff=date2-date1
  return datediff.days
+
+date1=input("please enter date 1:")
+date2=input("pleasea enter date 2:")
+
+try:
+    date1=datetime.datetime.strptime(date1,"%Y-%m-%d").date()
+    date2=datetime.datetime.strptime(date2,"%Y-%m-%d").date()
+    print(diference(date1,date2))
+except ValueError:
+    print("Please enter date in the format yyyy-mm-dd")
+    
