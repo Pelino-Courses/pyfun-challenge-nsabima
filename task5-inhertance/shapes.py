6b0a5dafcac72f10941eb719accee2d443065f62
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:19:47 +0200

    finding the area and rectangle of circle...

--- task5-inhertance/shapes.py
+++ task5-inhertance/shapes.py
@@ -23,3 +23,39 @@ class Circle(shape):
         return 2 *pi * self.radius
     def __str__(self):
         return f"{self.name}(radius={self.radius})"
+class Rectangle(shape):
+    def __init__(self, width:float,height=float):
+        super().__init__("rectangle")
+        if width<=0 or height<=0:
+            raise ValueError("width and height must be positive")
+        self.width=width   
+        self.height=height
+    def area(self):
+        return self.width*self.height
+    def perimeter(self):
+        return 2*(self.width+self.height)
+class Triangle(shape):
+    def __init__(self, a:float,b:float,c:float):
+        super().__init__("Triangle")
+        if any (side <=0 for side in (a,b,c)):
+            raise ValueError("all side should be positive")
+        if a+b <=c or a+c<=b or b+c<=a:
+            raise ValueError("invalide side length")
+        self.a=a
+        self.b=b
+        self.c=c
+    def area(self):
+        s=self.perimeter()/2
+        return sqrt(s*(s-self.a)*(s-self.b)*(s-self.c))
+    def perimeter(self):
+        return self.a+self.b+self.c
+    def __str__(self):
+        return f"{self.name}(a={self.a},b={self.b},c={self.c})"
+    # @classmethod
+shapes=[ 
+    Circle(5),
+    Rectangle(4,6),
+    Triangle(3,4,5),
+    ]
+for shape in shapes:
+    print(f"{shape}-> Area:{shape.area():.2f},perimeter:{shape.perimeter():.2f}")
