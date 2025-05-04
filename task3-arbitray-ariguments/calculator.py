b7d40f965c8a46c7de0816f8ae8f2cc14b800a45
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:08:26 +0200

    making simple calculator

--- task3-arbitray-ariguments/calculator.py
+++ task3-arbitray-ariguments/calculator.py
@@ -22,3 +22,63 @@ def divide(*args):
             raise ZeroDivisionError("cannot divide by zero")
         result /=num
     return result
+    def calculate(*args,**kwargs):
+    
+    if not all(isinstance(arg,int) for arg in args):
+        raise TypeError("all arguments shuld be numbers.")
+    print("The kwargs are: ",kwargs)
+    if kwargs.get('add'):
+        return add(*args)
+    elif kwargs.get('subtract'):
+        return substract(*args)
+    elif kwargs.get('multiply'):
+        return multiply(*args)
+    elif kwargs.get('divide'):
+        return divide(*args) 
+    else :
+        raise ValueError("invalid operation")
+    
+    
+# print(calculate(3,4,5, operation="add"))
+    
+if __name__=="__main__":
+    test_cases=[(10,5,2),(100,10),(8,2,0),('a',2)]
+    operations = [
+        {
+        "add": False,
+        "subtract": True,
+        "multiply": False,
+        "divide": False,
+        
+    },
+        {
+        "add": True,
+        "subtract": False,
+        "multiply": False,
+        "divide": False,
+        
+    },
+        {
+        "add": False,
+        "subtract": False,
+        "multiply": False,
+        "divide": True,
+        
+    },
+        {
+        "add": False,
+        "subtract": False,
+        "multiply": True,
+        "divide": True,
+        
+    },
+    ]
+    for op in operations:
+        print("\noperation: {op}")
+        for case in  test_cases:
+            try :
+                result=calculate(*case, **op)
+                print(f"calculate{case},operation='{op}=>{result}")
+            except Exception as e:
+                print(F"Error with input{case}:{e}") 
+                
