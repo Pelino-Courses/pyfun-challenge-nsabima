95e96fa074b708f30549060f51a865ff109f5cde
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:28:57 +0200

    when and how  the student are enrolled

--- task6-advanced-oop/enrollment.py
+++ task6-advanced-oop/enrollment.py
@@ -1,1 +1,10 @@
-
+from student import Student
+from course import Course
+class Enrollment(Student, Course):
+    def __init__(self, student_id, student_name, student_age, student_sex,course_id, course_name, date):
+        Student.__init__(self, student_id, student_name, student_age, student_sex)
+        Course.__init__(self, course_id, course_name)
+        self.date = date
+    def __str__(self):
+        return (f"Enrollment Date: {self.date} \nStudent: {self.id}\nCourse: {self.course_name}")
+    
