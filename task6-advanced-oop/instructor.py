94199d58d4c2017ce9a5bb782ea5dea6ebf15c7d
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:29:59 +0200

    info about instructor

--- task6-advanced-oop/instructor.py
+++ task6-advanced-oop/instructor.py
@@ -1,1 +1,8 @@
-
+from person import Person
+class Instructor(Person):
+    def __init__(self, id, name, age, sex, instructor_course):
+        super().__init__(id, name, age, sex)
+        self.instructor_course = instructor_course
+    def __str__(self):
+        return (f"Instructor: {self.name}\n Course: {self.instructor_course}")
+    
