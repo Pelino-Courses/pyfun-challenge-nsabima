e04a5fccbdb7a5ccd509e6185f2f24f5d2b012b4
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:07:02 +0200

    checking value and type error that can be raised

--- task3-arbitray-ariguments/calculator.py
+++ task3-arbitray-ariguments/calculator.py
@@ -1,1 +1,24 @@
 
+def add(*args):
+    return sum(args)
+def substract(*args):
+    if len(args) < 2:
+        raise ValueError("substraction needs two numbers.")
+    result=args[0]
+    for num in args[1:]:
+        result-=num
+    return result
+def multiply(*args):
+    result=1
+    for num in args:
+        result*=num
+    return result
+def divide(*args):
+    if len (args)<2:
+        raise ValueError("divide needs at least two numbers.")
+    result=args[0]
+    for num in args[1:]:
+        if num== 0:
+            raise ZeroDivisionError("cannot divide by zero")
+        result /=num
+    return result
