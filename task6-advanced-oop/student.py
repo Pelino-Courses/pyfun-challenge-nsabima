7155af8db3d5230b481a74dde2f67b3f10efe411
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:32:22 +0200

    student info to be enrolled

--- task6-advanced-oop/student.py
+++ task6-advanced-oop/student.py
@@ -1,1 +1,7 @@
-
+from person import Person
+class Student(Person):
+    def __init__(self,student_id:int, student_name:str, student_age:int, student_sex:str):
+        super().__init__(student_id, student_name, student_age, student_sex)
+    def __str__(self):
+        return (f"StudentId: {self.id} \n Name: {self.name}")
+    
