495dfbabf93b8ad66ce16bfd009269c45094e51f
Eric_IT <ericnsabimana2001@gmail.com>
2025-05-04 18:33:33 +0200

    information about teaching assistance

--- task6-advanced-oop/teaching_assistance.py
+++ task6-advanced-oop/teaching_assistance.py
@@ -1,1 +1,8 @@
-
+from instructor import Instructor
+from student import Student 
+class Teaching_Assistant(Instructor, Student):
+    def __init__(self,ta_id, ta_name, ta_age, ta_sex, ta_course):
+        Student.__init__(ta_id, ta_name, ta_age, ta_sex)
+        Instructor.__init__(ta_id, ta_name, ta_age, ta_sex, ta_course)
+    def __str__(self):
+        return (f"Name: {self.name}")
